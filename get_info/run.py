cd31c0c9db4df81c6b0b87250285b13f3651c7e7
Adam Imdieke <adam.imdieke3691@gmail.com>
2022-10-11 11:29:05 -0500

    Updated run.py to read JSON

--- get_info/run.py
+++ get_info/run.py
@@ -1,5 +1,8 @@
 #!/bin/env python3
 
-import os
+import os, json
 
-os.system("lshw -json > temp.txt")
+os.system("lshw -json > /tmp/temp.txt")
+data = json.load(open("/tmp/temp.txt"))
+
+print(data)
