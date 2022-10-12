ff1f26d0d3ef68c28a455c1985101193e4e98c05
Adam Imdieke <adam.imdieke361@gmail.com>
2022-10-12 14:32:52 -0500

    Removed tempfs folder

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo umount ./benchmarks/tempfs/ 2>/dev/null
-sudo mount -t tmpfs -o size=1G tmpfs ./benchmarks/tempfs/ -o nonempty
+sudo mount -t tmpfs -o size=1G tmpfs ./benchmarks/tempfs/ 
 
