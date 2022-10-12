00e12ae07a069649d004bc13fee44bbfb2b0b982
Adam Imdieke <adam.imdieke361@gmail.com>
2022-10-12 16:44:52 -0500

    Updated run.py

--- get_info/run.py
+++ get_info/run.py
@@ -1,8 +1,28 @@
 #!/bin/env python3
 
+#os.system("chown -R $USER:wheel " + root)
+
 import os, json
 
-os.system("lshw -json > /tmp/temp.txt")
+root = os.getcwd()
+
+assert os.getuid() == 0, "Error: Run as superuser"
+
+os.system("lshw -json -enable test -quiet > /tmp/temp.txt")
 data = json.load(open("/tmp/temp.txt"))
 
-print(data)
+cpu = data[0]['children'][0]['children'][0]['product']
+path = './data/' + data[0]['version'].replace(' ', '') + '/'
+
+assert os.path.isdir('./data'), "Error: Run in root directory!"
+if not (os.path.isdir(path)):
+    print("Adding dir: " , path)
+    os.mkdir(path)
+
+os.chdir(path)
+print("Entering dir: " + path)
+os.system("lshw -json -enable test -quiet > ./computer_info.json")
+print("\tDetected CPU: " + cpu)
+print("\tUpdated: " + path + "computer_info.json")
+
+
