87b98e8778f59e46bfa8edf8abf5c3ae14a55a0d
Adam Imdieke <adam.imdieke361@gmail.com>
2022-10-12 14:44:50 -0500

    Updated tmpfs.sh to unmount with any peramiter

--- init_tmpfs.sh
+++ init_tmpfs.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-mkdir -p ./benchmarks/tempfs
 sudo umount ./benchmarks/tempfs/ 2>/dev/null
-sudo mount -t tmpfs -o size=1G tmpfs ./benchmarks/tempfs/ 
+rm -r ./benchmarks/tempfs/
+mkdir -p ./benchmarks/tempfs
 
+if [ -z "$1" ] 
+then
+	sudo mount -t tmpfs -o size=1G tmpfs ./benchmarks/tempfs/ 
+fi
