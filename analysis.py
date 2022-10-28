cb540459eabe87dc1748b1302ae5a729b5c0c4ca
Adam Imdieke <adam.imdieke3691@gmail.com>
2022-10-28 12:01:59 -0500

    Modified analysis.py

--- analysis.py
+++ analysis.py
@@ -4,6 +4,7 @@
 # then enables data analysis on the data such as by creating plots
 
 import os, csv
+import matplotlib as mat
 
 def read_data(fname):
     run = {}
@@ -17,22 +18,22 @@ def main():
     machine = input("Enter the machine to get data for: ")
     test_foldr = input("Enter the folder containing all test data: ")
 
-    foldr = root + "/data/" + machine + "/" + test_foldr "/"
+    foldr = root + "/data/" + machine + "/" + test_foldr + "/"
 
     data = dict()
 
     # get list of compiler folders
-    compilers = list(filter(isdir, os.listdir(foldr)))
+    compilers = list(filter(os.path.isdir, os.listdir(foldr)))
     for c in compilers:
         data[c] = dict()
         # get list of flag folders
-        flags = list(filter(isdir, os.listdir(foldr + c)))
+        flags = list(filter(os.path.isdir, os.listdir(foldr + c)))
         for f in flags:
             data[c][f] = dict()
             
             for pf in [True, False]:
                 # get all csv files with stat data
-                stats = list(filter(lambda x: x[-3:] = "csv", 
+                stats = list(filter(lambda x: x[-3:] == "csv", 
                                     os.listdir(foldr + c + "/" + f + "/" + str(pf))
                                     ))
                 data[c][f][pf] = [read_data(f) for f in stats]
@@ -40,6 +41,13 @@ def main():
     # data should now exist
     # access via data[compiler][flag][prefetch bool][index][desired data field]
 
+    print(data)
+    x = []
+    y = []
+    for i in data["gcc"]["-O1"]["True"]:
+        x += i['branches'][0]
+        y += i['branch-misses'][0]
+    mat.pyplot.scatter(x, y)
+    mat.pyplot.show()
 
-
-
+main()
