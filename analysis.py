598dd4b354b7f98c8365e3c55dc56e64f69aa26c
Adam Imdieke <adam.imdieke3691@gmail.com>
2022-10-30 17:35:28 -0500

    Added not O0 plot

--- analysis.py
+++ analysis.py
@@ -49,7 +49,7 @@ def main():
 
     x_cat = 'L1-dcache-loads'
     y_cat = 'L1-dcache-load-misses'
-    parameter = (("-O0", False), ("-O0", True), ("-O1", False), ("-O1", True), ("-O2", False), ("-O2", True), ("-Ofast", False), ("-Ofast", True))
+    parameter = (("-O1", False), ("-O1", True), ("-O2", False), ("-O2", True), ("-Ofast", False), ("-Ofast", True))
     for parm in parameter:
         x = []
         y = []
