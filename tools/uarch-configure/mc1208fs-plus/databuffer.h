c52beae9850dacef8dec86eb449d06576b44c410
Adam Imdieke <adam.imdieke3691@gmail.com>
2023-04-05 09:11:26 -0500

    Clean old files

--- tools/uarch-configure/mc1208fs-plus/databuffer.h
+++
@@ -1,5 +0,0 @@
-struct dataBuffer_t {
-	unsigned int currIndex;
-	unsigned short *data;
-        unsigned int numPoints;
-};
--- tools/uarch-configure/mc1208fs-plus/poll_thread.h
+++
@@ -1,5 +0,0 @@
-int startContinuousTransfer(struct MCCDevice_t *dev,
-			struct dataBuffer_t *buffer,
-                        int samps, double delay);
-void stopContinuousTransfer(void);
-
